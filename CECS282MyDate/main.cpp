60b2c522a80fabd02128d14ab3fa2843fac5326f
Julia Navarro <79175907+JuliaVNavarro@users.noreply.github.com>
2021-11-22 15:55:02 -0800

    Update main.cpp

--- CECS282MyDate/main.cpp
+++ CECS282MyDate/main.cpp
@@ -1,10 +1,3 @@
-//  Navarro, Julia
-//  Class (CECS 282-02)
-//  Prog 2 – myDate Object
-//  Due 9/27/21
-// 
-//  I certify that this program is my own original work. I did not copy any part of this program from 
-//  any other source. I further certify that I typed each and every line of code in this program.
 
 #include <iostream>
 #include "myDate.h"
